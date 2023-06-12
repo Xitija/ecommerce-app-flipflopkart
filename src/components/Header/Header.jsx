c3fc78773b0b1178baa6cc6bca3d23327de4a6d4
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-13 01:23:40 +0530

    chore : added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import { NavLink, Routes, Route } from "react-router-dom";
+import { CiHeart, CiShoppingBasket, CiUser, CiSearch } from "react-icons/ci";
+
+import "../Header/Header.css";
+import finallogo from "../../assets/finallogo.png";
+
+export const Header = () => {
+  return (
+    <nav className="nav-bar">
+      <NavLink to="/" style={{ display: "contents" }}>
+        <img
+          className="logo"
+          style={{ width: "15%" }}
+          src={finallogo}
+          alt="Flipflopkart_logo"
+        />
+      </NavLink>
+      <div className="wrapper" style={{ width: "30%" }}>
+        <div class="icon">
+          <CiSearch />
+        </div>
+        <input
+          class="input"
+          type="text"
+          placeholder="Search Flipflops"
+          name="search"
+        />
+      </div>
+      <div className="navigation">
+        <NavLink to="/wishlist" className="nav-link">
+          <CiHeart color="#fc800b" size={25} strokeWidth="1.2" />
+        </NavLink>
+        <NavLink to="/cart" className="nav-link">
+          <CiShoppingBasket color="#fc800b" size={25} strokeWidth="1.2" />
+        </NavLink>
+        <NavLink to="/account" className="nav-link">
+          <CiUser color="#fc800b" size={25} strokeWidth="1.2" />
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
