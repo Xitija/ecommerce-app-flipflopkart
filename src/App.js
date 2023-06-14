b4d63b188eb7be8bfc5478ac85471018f0d71786
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-14 21:21:12 +0530

    refactor : removed unwanted code in App Component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,31 @@
 import { Routes, Route, NavLink } from "react-router-dom";
 
-// import "./App.css";
-
-import { InputText } from "primereact/inputtext";
-import { Button } from "primereact/button";
-import { Toast } from "primereact/toast";
-
-import "primereact/resources/themes/lara-light-teal/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
+import "./App.css";
+import { Header } from "./components/Header/Header";
 
 import Mockman from "mockman-js";
 import { Products } from "./pages/Products";
 import { Auth } from "./pages/Auth";
+import { Landing } from "./pages/Landing/Landing";
+import { Error } from "./pages/Error/Error";
 import logo from "./logo.png";
-import { useState , useRef} from "react";
+import { useState, useRef } from "react";
 
 function App() {
-  const [text, setText] = useState("");
-  const toastRef = useRef();
-
-  const onBtnClick = () => {
-    if (text) {
-      toastRef.current.show({
-        severity: "info",
-        summary: "Success",
-        detail: text,
-      });
-    } else {
-      toastRef.current.show({
-        severity: "error",
-        summary: "Error",
-        detail: "No data to display",
-      });
-    }
-  };
-
   return (
     <div className="App">
-      <Toast ref={toastRef} />
-      <h1>Project 1</h1>
-
-      <span className="p-float-label">
-      <InputText id="input_txt" value={text} onChange={(e) => setText(e.target.value)} />
-      <label htmlFor="input_txt">Enter Name</label>
-      </span>
-      <p>{text}</p>
-      <Button
-        type="button"
-        label="Submit"
-        icon="pi pi-check"
-        onClick={onBtnClick}
-      />
-      <Routes>
-        <Route path="/products" element={<Products />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/mockman" element={<Mockman />} />
-      </Routes>
+      <header>
+        <Header />
+      </header>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/mockman" element={<Mockman />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
     </div>
   );
 }
