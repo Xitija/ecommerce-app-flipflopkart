2e41572eeffdf8e6fe73fc0cb1239edabfc00698
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-17 01:48:53 +0530

    refactor : seperated filters from data context

--- src/reducers/DataReducer.js
+++ src/reducers/DataReducer.js
@@ -1,11 +1,6 @@
 export const initialDataState = {
   categories: [],
   products: [],
-  selectedCategories: [],
-  priceLessThan: 500,
-  selectedRating: 0,
-  selectedSort: "",
-  search: "",
 };
 
 export const dataReducer = (state, action) => {
@@ -20,44 +15,7 @@ export const dataReducer = (state, action) => {
         ...state,
         products: action.payload,
       };
-    case "SET_PRICE_LESS_THAN":
-      return {
-        ...state,
-        priceLessThan: action.payload,
-      };
-    case "SELECT_CATEGORY":
-      const categoryFound = state.selectedCategories.find(
-        (category) => action.payload === category
-      );
-      if (!categoryFound) {
-        return {
-          ...state,
-          selectedCategories: [...state.selectedCategories, action.payload],
-        };
-      } else {
-        return {
-          ...state,
-          selectedCategories: state.selectedCategories.filter(
-            (category) => category !== action.payload
-          ),
-        };
-      }
-      break;
-    case "SELECT_RATING":
-      return { ...state, selectedRating: Number(action.payload) };
-    case "SELECT_SORT":
-      console.log(action.payload,"apl")
-      return { ...state, selectedSort: action.payload };
-    case "CLEAR_FILTERS":
-      return {
-        ...state,
-        selectedCategories: [],
-        priceLessThan: 500,
-        selectedRating: 0,
-        selectedSort: "",
-        search: "",
-      };
-    default:
-      return { ...state };
+      default:
+        return { ...state };
   }
 };
