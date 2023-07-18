a0f2591b3f5a628a953fe78dd86744e737fd1229
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-18 20:40:34 +0530

    chore : added productlist component

---
+++ src/components/ProductsList/ProductsList.jsx
@@ -0,0 +1,24 @@
+import { useFilters } from "../../contexts/FiltersContext";
+import { ProductCard } from "../ProductCard/ProductCard";
+import "./ProductsList.css";
+
+export const ProductsList = () => {
+  const { getProductList } = useFilters();
+
+  const productList = getProductList();
+  return (
+    <div>
+      <div className="product-list-header">
+        <label>Showing Total {productList.length} Flip-Flops</label>
+      </div>
+      <div className="product-list">
+      {productList.map(
+        (product) => (
+          <ProductCard
+          key={product._id} product={product}/>
+        )
+      )}
+      </div>
+    </div>
+  );
+};
