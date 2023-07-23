7bd1d597e13c3afd9796315db341d78975f7e9e5
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-23 19:43:07 +0530

    chore : added images for empty cart and wishlist

--- src/assets/pictures.js
+++ src/assets/pictures.js
@@ -1,21 +1,25 @@
-import finallogo from "./finallogo.png"
+import finallogo from "./finallogo.png";
 import flipflop_banner from "./flipflop_banner.jpg";
 import flipflop_banner2 from "./flipflop_banner2.jpeg";
 import flipflop_banner3 from "./flipflop_banner3.jpg";
 import flipflops_men from "./flipflops_men.jpg";
 import flipflops_women from "./flipflops_women.jpg";
-import flipflops_kids from "./flipflops_kids.jpg"
+import flipflops_kids from "./flipflops_kids.jpg";
 import star from "./star.svg";
+import wishlistempty from "./wishlistempty.gif";
+import cartempty from "./cartempty.png";
 import errorimg from "./errorimg.gif";
 
 export {
-    finallogo,
-    flipflop_banner,
-    flipflop_banner2,
-    flipflop_banner3,
-    flipflops_men,
-    flipflops_women,
-    flipflops_kids,
-    star,
-    errorimg
-}
+  finallogo,
+  flipflop_banner,
+  flipflop_banner2,
+  flipflop_banner3,
+  flipflops_men,
+  flipflops_women,
+  flipflops_kids,
+  star,
+  wishlistempty,
+  cartempty,
+  errorimg
+};
