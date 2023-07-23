02c039532e43c734df70659368b72f5429e890fa
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-23 19:54:23 +0530

    chore: added profile component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,24 @@
+import { useAuth } from "../../contexts/AuthContext";
+
+import "./Profile.css";
+
+export const Profile = () => {
+  const { userAuth,logoutUser } = useAuth();
+  const { loggedInUser } = userAuth;
+
+  console.log(loggedInUser);
+
+  return (
+    <div className="profile-container">
+      <p>
+        <label className="label">Name:</label>
+        {" " + loggedInUser.firstName} {loggedInUser.lastName}
+      </p>
+      <p>
+        <label className="label">Email:</label>
+        {" " + loggedInUser.email}
+      </p>
+      <button className="logout-btn" onClick={logoutUser}>Logout</button>
+    </div>
+  );
+};
