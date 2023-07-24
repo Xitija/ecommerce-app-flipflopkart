217ebd52efb6391ec8ac56d8648cd33b11557bd9
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-24 23:36:28 +0530

    chore: added wishlist component

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,19 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { useProduct } from "../../contexts/ProductContext";
 import { wishlistempty } from "../../assets/pictures";
 
+import { ProductCard } from "../../components/ProductCard/ProductCard";
+
 import "./Wishlist.css";
+
 export const Wishlist = () => {
+  const { wishlist } = useProduct();
+
   return (
     <div>
-      <NavLink to="/products" className="wishlist_empty">
-        <img
-          className="img_wishlist"
-          src={wishlistempty}
-          alt="Wishlist_Empty"
-        />
-        <p>Tap the heart icon on the product to save it here</p>
-        <button>Start Wishlisting!</button>
-      </NavLink>
+      {wishlist.length && (
+        <div className="product-list">
+          {wishlist.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      )}
+      {wishlist.length === 0 && (
+        <NavLink to="/products" className="wishlist_empty">
+          <img
+            className="img_wishlist"
+            src={wishlistempty}
+            alt="Wishlist_Empty"
+          />
+          <p>Tap the heart icon on the product to save it here</p>
+          <button>Start Wishlisting!</button>
+        </NavLink>
+      )}
     </div>
   );
 };
