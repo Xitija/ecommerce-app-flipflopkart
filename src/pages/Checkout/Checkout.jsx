3d4c05005eb77cd1fe5ff195111c29fde70a4031
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-26 14:05:48 +0530

    chore : added checkout component

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,3 +1,83 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useProduct } from "../../contexts/ProductContext";
+import { CheckOutDetails } from "../../components/CheckOutDetails/CheckOutDetails";
+
+import "./Checkout.css";
+
 export const Checkout = () => {
-    return "Chkout"
-}
+  const navigate = useNavigate();
+  const { address } = useProduct();
+  const [deliveryAddress, setDeliveryAddress] = useState(
+    address[0] ? address[0] : {}
+  );
+  console.log(address);
+
+  const handleAddress = (selectAddress) => {
+    setDeliveryAddress(selectAddress);
+  };
+
+  return (
+    <div>
+      <div>
+        {address.length > 0 && (
+          <div className="checkout">
+            <div className="select-address">
+              <h2>Select Address</h2>
+              {address.map((singleAddress) => (
+                <div className="address-detail-card" key={singleAddress.id}>
+                  <p>{singleAddress.name}</p>
+                  <p>
+                    <>
+                      {singleAddress.house} {", "}
+                    </>
+                    <>
+                      {singleAddress.city} {", "}
+                    </>
+                    <>
+                      {singleAddress.state} {", "}
+                    </>
+                    <>{singleAddress.country} </>
+                  </p>
+                  <p>Pincode : {singleAddress.pincode}</p>
+                  <p>Phone: {singleAddress.mobile}</p>
+                  <button
+                    className={
+                      deliveryAddress.id === singleAddress.id
+                        ? "selected-btn"
+                        : "non-selected-btn"
+                    }
+                    onClick={() => handleAddress(singleAddress)}
+                  >
+                    {deliveryAddress.id === singleAddress.id
+                      ? "SELECTED"
+                      : "SELECT"}
+                  </button>
+                </div>
+              ))}
+              <button
+                className="add-new-address"
+                onClick={() => navigate("/account/address")}
+              >
+                Add new Address
+              </button>
+            </div>
+            <CheckOutDetails deliveryAddress={deliveryAddress} />
+          </div>
+        )}
+      </div>
+      {!address.length && (
+        <div className="proceed-checkout">
+          <p>Please Add Address to Proceed Checkout</p>
+          <button
+            className="add-new-address"
+            onClick={() => navigate("/account/address")}
+          >
+            Add New Address
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
