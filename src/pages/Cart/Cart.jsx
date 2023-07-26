d82e76d2e3047006a0bfa8efc4d96a6bdf653f27
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-26 09:33:01 +0530

    chore : added functionality to show cards

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,12 +1,32 @@
 import { NavLink } from "react-router-dom";
 import { cartempty } from "../../assets/pictures";
 
+import { useProduct } from "../../contexts/ProductContext";
+import { CartProductCard } from "../../components/CartProductCard/CartProductCard";
 import "./Cart.css";
+import { PriceDetails } from "../../components/PriceDetails/PriceDetails";
+
 export const Cart = () => {
+  const { cart } = useProduct();
+
   return (
-      <NavLink to="/products" className="cart_empty">
-        <img className="img_cart" src={cartempty} alt="Cart_Empty" />
-        <button>Shop Flipflops</button>
-      </NavLink>
+    <div>
+      {cart.length > 0 && (
+        <div className="cart">
+          <div className="cart-product-list">
+            {cart.map((product) => (
+              <CartProductCard key={product._id} product={product} />
+            ))}
+          </div>
+          <PriceDetails />
+        </div>
+      )}
+      {cart.length === 0 && (
+        <NavLink to="/products" className="cart_empty">
+          <img className="img_cart" src={cartempty} alt="Cart_Empty" />
+          <button>Shop Flipflops</button>
+        </NavLink>
+      )}
+    </div>
   );
 };
