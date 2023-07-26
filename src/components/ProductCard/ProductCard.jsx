9a11dba2d369741924cd818b74b4aec19c2dca13
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-26 12:17:34 +0530

    chore : added styles and handled out of stock

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import { useProduct } from "../../contexts/ProductContext";
 
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart, AiFillStar } from "react-icons/ai";
 import "./ProductCard.css";
 
 export const ProductCard = ({ product }) => {
-  const { wishlist, handleWishlist } = useProduct();
+  const { wishlist, cart, handleWishlist, handleCart } = useProduct();
   const { category, rating, in_stock, sell_price, title, image, _id } = product;
 
   const wishlistedByUser = wishlist.some((product) => product._id === _id);
+
+  const inUserCart = cart.some(({ _id }) => _id === product._id);
 
   return (
     <Link className="product-card" to={`/product-details/${_id}`}>
@@ -17,20 +19,39 @@ export const ProductCard = ({ product }) => {
       </div>
       <div className="product-info">
         <p className="truncate">{title}</p>
+
+        <p>{category}</p>
+        <p>
+          {rating} <AiFillStar />
+        </p>
+        <p>
+          {"₹ "}
+          {sell_price}
+        </p>
       </div>
-      <p>{category}</p>
-      <p>{rating}</p>
-      <p>{in_stock ? "in stock" : "outofstock"}</p>
-      <p>{sell_price}</p>
-      <span
-        onClick={(e) => {
-          e.preventDefault();
-          handleWishlist(product, wishlistedByUser);
-        }}
-      >
-        {wishlistedByUser ? <AiFillHeart /> : <AiOutlineHeart />}
-      </span>
-      <button className="add-to-cart-btn">Add to Cart</button>
+      {in_stock && (
+        <button
+          className="add-to-cart-btn"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCart(product, inUserCart);
+          }}
+        >
+          {inUserCart ? "GO TO CART" : "ADD TO CART"}
+        </button>
+      )}
+      {!in_stock && (
+        <button className="add-to-cart-btn out-of-stock">OUT OF STOCK</button>
+      )}
+       <div
+        className="wishlist-icon-product"
+          onClick={(e) => {
+            e.preventDefault();
+            handleWishlist(product, wishlistedByUser);
+          }}
+        >
+          {wishlistedByUser ? <AiFillHeart /> : <AiOutlineHeart />}
+        </div>
     </Link>
   );
 };
